975d0e995d98b78ef3e2774444386834ed2fd838
hayakawat <hayakawat@istyle.co.jp>
2021-10-08 14:27:44 +0900

    add:基となるViewnの追加

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<dir>
+    <hello-world></hello-world>
+</dir>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld.vue"
+export default {
+    components: { 
+        HelloWorld
+    },
+    name: 'Home'
+}
+</script>
+
+<style>
+</style>
